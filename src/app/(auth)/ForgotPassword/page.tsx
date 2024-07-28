00818541344a292eeadc36c4ebfa32e3e99994f7
RizkyMaulanaS <rizkyzky345@gmail.com>
2024-07-28 13:45:03 +0700

    Make Nested Route for Auth and make a components

---
+++ src/app/(auth)/ForgotPassword/page.tsx
@@ -0,0 +1,8 @@
+export default function ForgotPassword(){
+    return (
+        <main>
+            <h1>Forgot</h1>
+        </main>
+    );
+
+}
---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,14 @@
+export default function NavigationBar(){
+    return (
+        <main>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Source</li>
+                    <li>Miaw</li>
+                </ul>
+            </nav>
+        </main>
+    );
+}
